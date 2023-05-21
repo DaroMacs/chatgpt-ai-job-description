70e22cc367831aec12f0792253f42ef73b14352c
DaroMacs <dariomacedog@gmail.com>
2023-05-20 21:54:30 -0600

    first commit

---
+++ src/pages/api/returnJobDescription.ts
@@ -0,0 +1,76 @@
+interface IGenerateDescription {
+  jobTitle: string;
+  industry: string;
+  keyWords: string;
+  tone: string;
+  numWords: string;
+}
+
+const generateDescription = async ({
+  jobTitle,
+  industry,
+  keyWords,
+  tone,
+  numWords,
+}: IGenerateDescription): Promise<any> => {
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/engines/text-davinci-003/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          prompt: `Write a job description for a  ${jobTitle} role 
+            ${industry ? `in the ${industry} industry` : ""} that is around ${
+            numWords || 200
+          } words in a ${tone || "neutral"} tone. ${
+            keyWords ? `Incorporate the following keywords: ${keyWords}.` : ""
+          }. The job position should be described in a way that is SEO friendly, highlighting its unique features and benefits.`,
+          max_tokens: 100,
+          temperature: 0.5,
+        }),
+      }
+    );
+    const data = await response.json();
+
+    return data.choices[0].text;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export default async function handler(
+  req: {
+    body: {
+      jobTitle: any;
+      industry: any;
+      keyWords: any;
+      tone: any;
+      numWords: any;
+    };
+  },
+  res: {
+    status: (arg0: number) => {
+      (): any;
+      new (): any;
+      json: { (arg0: { jobDescription: any }): void; new (): any };
+    };
+  }
+) {
+  const { jobTitle, industry, keyWords, tone, numWords } = req.body;
+
+  const jobDescription = await generateDescription({
+    jobTitle,
+    industry,
+    keyWords,
+    tone,
+    numWords,
+  });
+
+  res.status(200).json({
+    jobDescription,
+  });
+}
